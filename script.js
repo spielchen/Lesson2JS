00cebee00c03546e0ac4a7f2837b92d91a6a44f0
spielchen <anna.bukavtsova@gmail.com>
2019-03-02 18:30:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-		money,
-		time, 
+		budget: money,
+		timeData: time, 
 		expenses: {},
 		income:[],
 		optionalExpenses:{},
@@ -24,7 +24,7 @@ let appData = {
 				}
 		};
 
-		appData.moneyPerDay = appData.money / 30;
+		appData.moneyPerDay = appData.budget / 30;
 		alert("Ежедневный бюджет: " + appData.moneyPerDay);
 		if (appData.moneyPerDay < 100){
 			console.log('Минимальный уровень достатка')
